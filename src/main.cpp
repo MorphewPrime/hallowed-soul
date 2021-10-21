c75e722bf0699959baea812952f9fff109730cf3
MorphewPrime <jackmorphew101@gmail.com>
2021-10-21 15:39:24 -0500

    basic code for sprite animation

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,14 @@ int main()
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    sf::Clock clock;
+    sf::Texture texture;
+    texture.loadFromFile("src/Images/sprite_test.png");
+
+    sf::IntRect rectSourceSprite(0, 488, 90, 98);
+    sf::Sprite sprite(texture, rectSourceSprite);
+
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -17,8 +25,27 @@ int main()
                 window.close();
         }
 
+        bool rowCount = 1;
+        if(clock.getElapsedTime().asSeconds() > 0.1f)
+        {
+            if (rectSourceSprite.left == 810)
+            {
+                rectSourceSprite.left = 0;
+                rowCount++;
+            } else {
+                rectSourceSprite.left += 90;
+            }
+
+            // rectSourceSprite.top = ((rowCount % 4)) * 98;
+
+            sprite.setTextureRect(rectSourceSprite);
+            clock.restart();
+
+        }
+
         window.clear();
         window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
 
